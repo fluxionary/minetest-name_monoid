49a129aafb3e25e70f351a3c9f3665e3ef30c354
flux <25628292+fluxionary@users.noreply.github.com>
2022-11-29 11:57:14 -0800

    fmod and normalization

--- init.lua
+++ init.lua
@@ -1,27 +1,5 @@
-local modname = minetest.get_current_modname()
-local modpath = minetest.get_modpath(modname)
-local S = minetest.get_translator(modname)
+name_monoid = fmod.create()
 
-name_monoid = {
-	version = os.time({year = 2022, month = 5, day = 22}),
-	fork = "fluxionary",
-
-	modname = modname,
-	modpath = modpath,
-
-	S = S,
-
-	log = function(level, message_fmt, ...)
-		local message = message_fmt:format(...)
-		minetest.log(level, ("[%s] %s"):format(modname, message))
-	end,
-
-	dofile = function(...)
-		dofile(table.concat({modpath, ...}, DIR_DELIM) .. ".lua")
-	end,
-}
-
-name_monoid.dofile("settings")
 name_monoid.dofile("monoid")
 
 minetest.register_on_joinplayer(function(player)
--- settings.lua
+++
@@ -1,7 +0,0 @@
-local s = minetest.settings
-
-name_monoid.settings = {
-    tag_separator = s:get("name_monoid.component_separator") or " ",
-    show_name = s:get_bool("name_monoid.show_name", true),
-    invert_composition = s:get_bool("name_monoid.invert_composition", false),
-}
